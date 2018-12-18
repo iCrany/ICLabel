187e811883f7a256a04c7bc710d4971f929b6ca4
iCrany <crany1992@gmail.com>
2018-12-18 19:57:25 +0800

    Support Carthage

---
+++ Carthage/ICAttributedLabel/ICAttributedLabel/ICAttributedLabel.h
@@ -0,0 +1,26 @@
+//
+//  ICAttributedLabel.h
+//  ICAttributedLabel
+//
+//  Created by iCrany on 2018/12/18.
+//  Copyright © 2018 iCrany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ICAttributedLabel.
+FOUNDATION_EXPORT double ICAttributedLabelVersionNumber;
+
+//! Project version string for ICAttributedLabel.
+FOUNDATION_EXPORT const unsigned char ICAttributedLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ICAttributedLabel/PublicHeader.h>
+
+#import <ICLabel/ICHighlight.h>
+#import <ICLabel/ICLabel.h>
+#import <ICLabel/ICLabelAttachment.h>
+#import <ICLabel/ICLabelDrawService.h>
+#import <ICLabel/ICLabelMarco.h>
+#import <ICLabel/ICLinkData.h>
+#import <ICLabel/NSAttributedString+ICLabel.h>
+#import <ICLabel/NSMutableAttributedString+ICLabel.h>
