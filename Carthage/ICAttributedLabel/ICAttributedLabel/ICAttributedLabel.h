198ed302d9992ff4c780f033af7073d95ab94cfb
iCrany <crany1992@gmail.com>
2019-01-17 19:23:58 +0800

    fix carthage public header issue

--- Carthage/ICAttributedLabel/ICAttributedLabel/ICAttributedLabel.h
+++ Carthage/ICAttributedLabel/ICAttributedLabel/ICAttributedLabel.h
@@ -16,11 +16,11 @@ FOUNDATION_EXPORT const unsigned char ICAttributedLabelVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ICAttributedLabel/PublicHeader.h>
 
-#import <ICLabel/ICHighlight.h>
-#import <ICLabel/ICLabel.h>
-#import <ICLabel/ICLabelAttachment.h>
-#import <ICLabel/ICLabelDrawService.h>
-#import <ICLabel/ICLabelMarco.h>
-#import <ICLabel/ICLinkData.h>
-#import <ICLabel/NSAttributedString+ICLabel.h>
-#import <ICLabel/NSMutableAttributedString+ICLabel.h>
+#import <ICAttributedLabel/ICHighlight.h>
+#import <ICAttributedLabel/ICLabel.h>
+#import <ICAttributedLabel/ICLabelAttachment.h>
+#import <ICAttributedLabel/ICLabelDrawService.h>
+#import <ICAttributedLabel/ICLabelMarco.h>
+#import <ICAttributedLabel/ICLinkData.h>
+#import <ICAttributedLabel/NSAttributedString+ICLabel.h>
+#import <ICAttributedLabel/NSMutableAttributedString+ICLabel.h>
